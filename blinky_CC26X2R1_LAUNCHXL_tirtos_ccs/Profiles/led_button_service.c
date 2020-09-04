0aa71401fc867b51cdd653d957af4b3b2053392f
Leo Sun <leosunsl@outlook.com>
2020-09-04 17:42:20 +0800

    led button service table

--- blinky_CC26X2R1_LAUNCHXL_tirtos_ccs/Profiles/led_button_service.c
+++ blinky_CC26X2R1_LAUNCHXL_tirtos_ccs/Profiles/led_button_service.c
@@ -4,6 +4,93 @@
  *  Created on: 2020Äê9ÔÂ4ÈÕ
  *      Author: ASUS
  */
+#include <string.h>
+
+//#include <xdc/runtime/Log.h> // Comment this in to use xdc.runtime.Log
+#include <ti/common/cc26xx/uartlog/UartLog.h>  // Comment out if using xdc Log
+
+#include <icall.h>
+
+/* This Header file contains all BLE API and icall structure definition */
+#include "icall_ble_api.h"
 
 #include "led_button_service.h"
 
+// LED_BUTTON_Service Service UUID
+CONST uint8_t LedButtonServiceUUID[ATT_UUID_SIZE] =
+{
+     LBS_UUID_BASE(LBS_UUID_SERVICE)
+};
+// Service declaration
+static CONST gattAttrType_t LBServiceDecl = { ATT_UUID_SIZE, LedButtonServiceUUID };
+
+static uint8_t lbs_ButtonProps = GATT_PROP_NOTIFY | GATT_PROP_READ;
+// BUTTON UUID
+CONST uint8_t buttonCharUUID[ATT_UUID_SIZE] =
+{
+ LBS_UUID_BASE(LBS_UUID_BUTTON_CHAR)
+};
+
+static uint8 BUTTONVal = 0;
+
+// Characteristic "BUTTON" Client Characteristic Configuration Descriptor
+static gattCharCfg_t *lbs_BUTTONConfig;
+
+// Characteristic "LED" Properties (for declaration)
+static uint8_t lbs_LEDProps = GATT_PROP_READ | GATT_PROP_WRITE |
+                              GATT_PROP_WRITE_NO_RSP;
+
+// LED UUID
+CONST uint8_t ledCharUUID[ATT_UUID_SIZE] =
+{
+ LBS_UUID_BASE(LBS_UUID_LED_CHAR)
+};
+
+static uint8 LEDVal = 0;
+
+static gattAttribute_t LB_ServiceAttrTbl[] =
+{
+    // LED_BUTTON_Service Service Declaration
+    {
+        { ATT_BT_UUID_SIZE, primaryServiceUUID },
+        GATT_PERMIT_READ,
+        0,
+        (uint8_t *)&LBServiceDecl
+    },
+    // Button Characteristic Declaration
+    {
+        { ATT_BT_UUID_SIZE, characterUUID },
+        GATT_PERMIT_READ,
+        0,
+        &lbs_ButtonProps
+    },
+    // Button Characteristic Value
+    {
+        { ATT_UUID_SIZE, buttonCharUUID },
+        GATT_PERMIT_READ,
+        0,
+        &BUTTONVal
+    },
+    // Button CCCD
+    {
+        { ATT_BT_UUID_SIZE, clientCharCfgUUID },
+        GATT_PERMIT_READ | GATT_PERMIT_WRITE,
+        0,
+        (uint8_t *)&lbs_BUTTONConfig
+    },
+
+    // LED Characteristic Declaration
+    {
+        { ATT_BT_UUID_SIZE, characterUUID },
+        GATT_PERMIT_READ,
+        0,
+        &lbs_LEDProps
+    },
+    // LED Characteristic Value
+    {
+        { ATT_UUID_SIZE, ledCharUUID },
+        GATT_PERMIT_READ | GATT_PERMIT_WRITE | GATT_PERMIT_WRITE,
+        0,
+        &LEDVal
+    },
+};
--- blinky_CC26X2R1_LAUNCHXL_tirtos_ccs/Profiles/led_button_service.h
+++ blinky_CC26X2R1_LAUNCHXL_tirtos_ccs/Profiles/led_button_service.h
@@ -8,7 +8,13 @@
 #ifndef PROFILES_LED_BUTTON_SERVICE_H_
 #define PROFILES_LED_BUTTON_SERVICE_H_
 
+#include <bcomdef.h>
 
+#define LBS_UUID_BASE(uuid)        0x23, 0xD1, 0xBC, 0xEA, 0x5F, 0x78, 0x23, 0x15, \
+                              0xDE, 0xEF, 0x12, 0x12, LO_UINT16(uuid), HI_UINT16(uuid), 0x00, 0x00
+#define LBS_UUID_SERVICE     0x1523
+#define LBS_UUID_BUTTON_CHAR 0x1524
+#define LBS_UUID_LED_CHAR    0x1525
 
 
 
